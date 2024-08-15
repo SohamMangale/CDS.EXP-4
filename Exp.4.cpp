bc865bc71a5d0296f5405659b171c66dcd0344d3
SohamMangale <sohammangale60@gmail.com>
2024-08-15 12:51:10 +0530

    Create Exp.4.cpp

---
+++ Exp.4.cpp
@@ -0,0 +1,25 @@
+//Soham
+//23070123084
+//entc b1
+//experiment 4
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5;  //101
+    int b=3;  // 011
+    //bitwise AND
+    int bitwise_and =a&b;
+    int bitwise_or =a| b;
+    int bitwise_xor =a ^b;
+    int bitwise_not =-a;
+    int left_shift =a<<2;
+    int right_shift =a>> 1;
+    cout<<"AND:"<<bitwise_and<<endl;
+    cout<<"OR:"<<bitwise_or<<endl;
+    cout<<"XOR:"<<bitwise_xor<<endl;
+    cout<<"NOT a:"<<bitwise_not<<endl;
+    cout<<"Left shift:"<<left_shift<<endl;
+    cout<<"Right shift:"<<right_shift<<endl;
+    return 0;
+}
